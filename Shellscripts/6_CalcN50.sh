b399444dfa315985c75ed991dd766e325a199cc0
oneplumtree <ginny.aiwa.li@gmail.com>
2025-08-29 13:00:57 -0600

    Update 6_CalcN50.sh

--- Shellscripts/6_CalcN50.sh
+++ Shellscripts/6_CalcN50.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#Trinity Salmon Abundance Estimation
+
+# .sh file can be found in:
 ###################################################
-#####Calculate the N50 of Assembled Transcriptomes
+#####Loop through directories to find fasta files
 ###################################################
 
-for f1 in /data/poppy/domesticationpoppy/hisat2/otherpoppy/*/;
+for f1 in /data/poppy/domesticationpoppy/ncbihisat/zeno/*trinity*/;
 
 do
         name=${f1%%-trinity.fasta} #only keep the front half of the name
         echo "Processing N50 stats for $name"
-         /data/programs/trinityrnaseq-v2.14.0/util/TrinityStats.pl "$name"/Trinity-GG.fasta > "$name"trinityN50stats.txt
-done
+         /data/programs/trinityrnaseq-v2.14.0/util/TrinityStats.pl "$name"/Trinity-GG.fasta #> "$name"trinityN50stats.txt
+done > zenotrinityN50Stats.txt #save them all in one file
+
